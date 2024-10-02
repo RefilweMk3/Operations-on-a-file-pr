36cdb1654054e404c537b7cfde70b2e7876a25ae
RefilweMk3 <refilwekgwadi13@gmail.com>
2024-10-02 13:57:34 +0200

    Operations on a file project

--- main.py
+++ main.py
@@ -30,21 +30,3 @@ for line in f1.readlines():
         f2.write(line)
 f1.close()
 f2.close()
-
-with open("ref.txt","w") as f:
-    f.write("Hey there \n how are u doing today")
-f.close
-with open("ref.txt","r")as f:
-    data = f.readlines()
-    for line in data:
-        word = line.split()
-        print(word)
-f.close()
-
-#file=open("kg.txt","x")
-#file.write("sample file")
-#file.close()
-
-#import os
-#os.remove("kg")
-#os.rmdir("kg")
